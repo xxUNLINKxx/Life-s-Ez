ba0c2cba9a24913bd5b8e2f853ff169567c120ac
xxUNLINKxx <49578656+xxUNLINKxx@users.noreply.github.com>
2020-02-01 13:29:11 -0800

    Transitions
    Transitions are ready to be used

--- Life's EZ/Assets/SceneTransition.cs
+++ Life's EZ/Assets/SceneTransition.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class SceneTransition : MonoBehaviour
 {
+
+    private bool hasPressed;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            StartCoroutine(StartScene(1f));
+            hasPressed = false;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }        
+        
+        if (Input.GetKeyDown(KeyCode.Space)&&!hasPressed)
+        {
+            hasPressed = true;
+            StartCoroutine(ExitScene(2f));
         }
+
     }
-    public IEnumerator StartScene(float time)
+
+    public IEnumerator ExitScene(float delay)
     {
-        LeanTween.moveLocalY(this.gameObject, 0, 1f).setEaseInOutSine();
-        yield return new WaitForSecondsRealtime(time);
-        LeanTween.scale(this.gameObject, new Vector2(20,20), 0.5f).setEaseInOutSine();
+        LeanTween.scaleY(this.gameObject, 0.1f, 1.2f).setEaseOutSine();
+        LeanTween.moveLocalY(this.gameObject, 0, 1.2f).setEaseOutSine().setEaseSpring();
+        yield return new WaitForSecondsRealtime(0.9f);
+        LeanTween.scale(this.gameObject, new Vector2(20,20), 1f).setEaseLinear();
+        yield return new WaitForSecondsRealtime(delay);
+        StartCoroutine(EnterScene());
+    }
+
+    public IEnumerator EnterScene()
+    {
+        LeanTween.scale(this.gameObject, new Vector2(0.1f, 0.1f), 1f).setEaseLinear();
+        yield return new WaitForSecondsRealtime(1f);
+        LeanTween.moveLocalY(this.gameObject, -1000, 0.8f).setEaseInSine().setEaseInBack();
+        LeanTween.scaleY(this.gameObject, 1f, 1f).setEaseInSine();
+        hasPressed = false;
     }
 }
